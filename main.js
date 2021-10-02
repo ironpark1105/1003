d1bed745a8a33187d7f33c1364be70086e44126c
ironpark1105 <85903711+ironpark1105@users.noreply.github.com>
2021-10-03 01:52:20 +0900

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+var imgUrl = [
+    'images/black.webp',
+    'images/green.webp',
+    'images/gray.webp'
+]
+
+
+
+
+
+
+function changeimage(num){
+    var image = imgUrl(num);
+    $('figure img').attr('sec',[num])
+
+}
